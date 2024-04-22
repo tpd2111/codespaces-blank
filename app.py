095ff2c9e7d11a744acdc9260ce6de7570b3501d
tpd2111 <122829848+tpd2111@users.noreply.github.com>
2024-04-22 18:57:41 +0000

    update

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import folium
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # Set a default location (Center of the UK)
+    start_coords = (54.2361, -4.5481)
+    folium_map = folium.Map(location=start_coords, zoom_start=6)
+
+    if request.method == 'POST':
+        postcode = request.form['postcode']
+        res = requests.get(f'http://api.postcodes.io/postcodes/{postcode}')
+        if res.status_code == 200:
+            data = res.json()
+            if data['status'] == 200:
+                lat = data['result']['latitude']
+                lon = data['result']['longitude']
+                folium.Marker([lat, lon], tooltip='Click for more', popup=postcode).add_to(folium_map)
+                # Center the map on the searched postcode
+                folium_map.location = [lat, lon]
+                folium_map.zoom_start = 13
+
+    # Render the map in the HTML template
+    folium_map = folium_map._repr_html_()
+
+    return render_template('map.html', folium_map=folium_map)
+
+if __name__ == '__main__':
+    app.run(debug=True)
